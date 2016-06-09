4ddaffd1c62087fddd0becb00c52a75f61009caf
tn <t.nurislamov@gmail.com>
2016-06-09 20:00:41 +0300

    Revert "try again"
    This reverts commit 9dc72ce9a1b9d35c6a4b87338fb233f9d7f2b090.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 require('use-strict');
-const http = require('http');
+
 const App = require('./app/router');
 
-var server = http.createServer(function(req, res) {
-    new App();
-});
-
-server.listen(8080);
+new App();
